b94b09ffc666354db2b4227ecd51b435fa241c04
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-24 19:45:47 +0000

    add create, update, delete gui for directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -36,4 +36,46 @@ class DirectorsController < ApplicationController
 
     render({ :template => "director_templates/eldest" })
   end
+
+  def create
+    new_director = Director.new
+
+    new_director.name = params.fetch("query_name")
+    new_director.dob = params.fetch("query_dob")
+    new_director.bio = params.fetch("query_bio")
+    new_director.image = params.fetch("query_image")
+
+    if new_director.valid?
+      new_director.save
+      redirect_to("/directors", { :notice => "Director created successfully." })
+    else
+      redirect_to("/directors", { :notice => "Director failed to create successfully." })
+    end
+  end
+
+  def destroy
+    delete_id = params.fetch("path_id")
+    delete_candidate = Director.where({id: delete_id}).at(0)
+
+    delete_candidate.destroy
+
+    redirect_to("/directors", { :notice => "Director deleted successfully." })
+  end
+
+  def update
+    update_id = params.fetch("path_id")
+    update_candidate = Director.where({id: update_id}).at(0)
+
+    update_candidate.name = params.fetch("query_name")
+    update_candidate.dob = params.fetch("query_dob")
+    update_candidate.bio = params.fetch("query_bio")
+    update_candidate.image = params.fetch("query_image")
+
+    if update_candidate.valid?
+      update_candidate.save
+      redirect_to("/directors/#{update_id}", { :notice => "Director updated successfully." })
+    else
+      redirect_to("/directors/#{update_id}", { :notice => "Director failed to update successfully." })
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,11 @@ Rails.application.routes.draw do
 
   get("/directors/youngest", { :controller => "directors", :action => "max_dob" })
   get("/directors/eldest", { :controller => "directors", :action => "min_dob" })
-
   get("/directors", { :controller => "directors", :action => "index" })
   get("/directors/:path_id", { :controller => "directors", :action => "show" })
+  post("/insert_director", { :controller => "directors", :action => "create" })
+  get("/delete_director/:path_id", { :controller => "directors", :action => "destroy"})
+  post("modify_director/:path_id", { :controller => "directors", :action => "update"})
 
   get("/movies", { :controller => "movies", :action => "index" })
   get("/movies/:path_id", { :controller => "movies", :action => "show" })
