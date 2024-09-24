b08294843290468ddd6ac97bc1bc9b220ac51ff3
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-24 22:12:30 +0000

    add delete_actor route

--- config/routes.rb
+++ config/routes.rb
@@ -18,4 +18,5 @@ Rails.application.routes.draw do
   get("/actors", { :controller => "actors", :action => "index" })
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
   post("/insert_actor", { :controller => "actors", :action => "create"})
+  get("/delete_actor", { :controller => "actors", :action => "destroy"})
 end
