e1a4f5e5abbcc620b121fe9aac319df82a59b16f
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-24 21:35:23 +0000

    new create method in moviescontroller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -14,4 +14,22 @@ class MoviesController < ApplicationController
 
     render({ :template => "movie_templates/show" })
   end
+
+  def create
+    new_movie = Movie.new
+
+    new_movie.title = params.fetch("query_title")
+    new_movie.year = params.fetch("query_year")
+    new_movie.duration = params.fetch("query_duration")
+    new_movie.description = params.fetch("query_description")
+    new_movie.image = params.fetch("query_image")
+    new_movie.director_id = params.fetch("query_director_id")
+
+    if new_movie.valid?
+      new_movie.save
+      redirect_to("/movies", { :notice => "Movie created successfully." })
+    else
+      redirect_to("/movies", { :notice => "Movie failed to create successfully." })
+    end
+  end
 end
