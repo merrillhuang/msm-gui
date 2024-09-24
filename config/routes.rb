0e48ee67e81466ccc0ed96d841b4844126753501
Merrill Huang <127697576+merrillhuang@users.noreply.github.com>
2024-09-24 22:17:28 +0000

    add modify_actor route

--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
   get("/actors/:path_id", { :controller => "actors", :action => "show" })
   post("/insert_actor", { :controller => "actors", :action => "create"})
   get("/delete_actor/:path_id", { :controller => "actors", :action => "destroy"})
+  post("/modify_actor/:path_id", { :controller => "actors", :action => "update"})
 end
